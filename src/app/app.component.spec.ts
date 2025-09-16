ca4ca020a10ec4dc4b37e046a3090ef1cdba89d5
peruclarice <claricekibii@gmail.com>
2025-09-16 22:57:45 +0300

    Restructuing into CareerNava

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zeraki' title`, () => {
+  it(`should have the 'Career Nava' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zeraki');
+    expect(app.title).toEqual('Career Nava');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zeraki');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Career Nava');
   });
 });
