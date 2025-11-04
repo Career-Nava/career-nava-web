f5f4a9313b6645ba09503a318ee91a6ed8e85a50
manlikeclaro <ckibii08@gmail.com>
2025-11-04 17:47:24 +0300

    chore: update environment configs and align API base URL with backend deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7128/api',
+  apiBaseUrl: 'https://net-8-rest-api-for-careernava.onrender.com/api',
   googleClientId: '339583299127-9tk18mok2u08nlvkjblp6cg1ao1r24eq.apps.googleusercontent.com'
 };
