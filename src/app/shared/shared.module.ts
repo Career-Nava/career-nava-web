4e0f749c95ba2e5e45734ad6105bb60413657099
manlikeclaro <ckibii08@gmail.com>
2026-06-29 18:11:54 +0300

    Revert "feat(web): add shared dashboard action button"
    This reverts commit 7ff2ae975fda89e60499385898560a7d61aaee27.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastContainerComponent } from '../modules/shared/toast-container/toast-container.component';
-import { ActionButtonComponent } from './components/action-button/action-button.component';
 
 @NgModule({
   declarations: [ToastContainerComponent],
@@ -16,7 +15,6 @@ import { ActionButtonComponent } from './components/action-button/action-button.
     ReactiveFormsModule,
     RouterModule,
     FontAwesomeModule,
-    ActionButtonComponent
   ],
   exports: [
     CommonModule,
@@ -25,7 +23,6 @@ import { ActionButtonComponent } from './components/action-button/action-button.
     ReactiveFormsModule,
     RouterModule,
     FontAwesomeModule,
-    ActionButtonComponent,
     ToastContainerComponent
   ]
 })
